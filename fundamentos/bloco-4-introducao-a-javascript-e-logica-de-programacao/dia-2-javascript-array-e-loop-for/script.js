2cafc88b8a2aa7491451485b44be068b464700b2
Henrique <henriquerios4@gmail.com>
2021-07-29 19:19:25 -0300

    exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menor = numbers[0];
+let impar = 0;
 
 for(let index = 0; index < numbers.length; index +=1){
    
-    if (menor > numbers[index]){
-        menor = numbers[index];
+    if (numbers[index] % 2 == 1){
+        impar += 1;
     }
 }
-console.log(menor);
+console.log(impar);
