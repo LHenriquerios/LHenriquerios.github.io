b48c06672c84108ce7009e92a69c333e16b112b0
Henrique <henriquerios4@gmail.com>
2021-07-29 18:59:12 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,9 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let maior = 0;
 
 for(let index = 0; index < numbers.length; index +=1){
    
-    soma = soma + numbers[index];
+    if (numbers[index] > maior){
+        maior = numbers[index];
+    }
 }
-
-console.log('A média aritmética é: ' + soma/numbers.length);
+console.log(maior);
